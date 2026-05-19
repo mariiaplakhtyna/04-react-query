17b099e8a503801cd43b60d646334eba93d98f27
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-05-19 10:43:42 -0400

    fix build

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -4,6 +4,8 @@ export interface Movie {
   poster_path: string;
   release_date: string;
   vote_average: number;
+  backdrop_path: string;
+overview: string;
 }
 
 export interface MoviesResponse {
