d360a051f9316e98ba781ca28d31a399597fa3ab
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-05-19 10:26:18 -0400

    Fix modern normalize import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'modern-normalize';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './components/App/App';
 
